561c5270a4df210e5ca661ce342e6d69138a3465
Aleksandr <aleksandr.boyushenko@mail.ru>
2021-11-18 16:26:13 +0300

    Upload PZ_6_3 without comments

---
+++ PZ_6/PZ_6_3.py
@@ -0,0 +1,21 @@
+# Дан список размера N (N — четное число). Поменять местами его первый элемент со
+# вторым, третий — с четвертым и т. д.
+
+from random import randint
+
+def change_obj(lim):
+    my_list = []
+    for i in range(lim):
+        num = randint(1, 100)
+        my_list.append(num)
+    print(f"Первоначальный список: {my_list}")
+    for i in range(0, len(my_list), 2):
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+    return my_list
+
+a = int(input(f"Введите чётный предел списка: "))
+while a % 2 != 0 or a < 0:
+    print(f"Вы ввели нечётное или отрицательное число")
+    a = int(input(f"Введите чётный пердел списка"))
+
+print(f"Конечный список: {change_obj(a)}")

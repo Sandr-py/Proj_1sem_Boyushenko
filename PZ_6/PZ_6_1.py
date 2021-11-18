2dc1c2f98c12face6dc4dcdea9fa8e1d6dfa7cb3
Aleksandr <aleksandr.boyushenko@mail.ru>
2021-11-18 16:05:28 +0300

    Upload PZ_6_1 and PZ_6_2 without comments

---
+++ PZ_6/PZ_6_1.py
@@ -0,0 +1,14 @@
+# Дан первый член A и разность D арифметической прогрессии. Сформировать и вывести
+# список размера 10, содержащий 10 первых членов данной прогрессии: A, A + D, A +
+# 2*D, A + 3*D, ... .
+
+def ArifProg(num, diff):
+    counter = 0
+    ListArif = []
+    while counter != 10:
+        summ = num + diff * counter
+        ListArif.append(summ)
+        counter += 1
+    return ListArif
+
+print(ArifProg(float(input(f"Введите первый член арифметической прогрессии:  ")), float(input(f"Введите разность:  "))))
---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,18 @@
+# Дан список размера N. Найти номер его первого локального минимума (локальный
+# минимум — это элемент, который меньше любого из своих соседей).
+
+from random import randint
+
+
+def min_lists_obj(lim):
+    my_list = []
+    for i in range(lim):
+        a = randint(1, 100)
+        my_list.append(a)
+    for i in range(len(my_list)):
+        if my_list[i] < my_list[i + 1] and my_list[i] < my_list[i - 1]:
+            return my_list[i], my_list
+
+
+LocMin, AllList = min_lists_obj(int(input(f"Введите предел списка   ")))
+print(f"Превый локальный минимум списка {AllList}: {LocMin}")

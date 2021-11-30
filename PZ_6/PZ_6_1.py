3d742251488b1205c214352c323bc3e901dec48d
Aleksandr <83352964+Sandr-py@users.noreply.github.com>
2021-11-30 21:46:25 +0300

    Update PZ_6_1 (adding comments)

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -2,7 +2,7 @@
 # список размера 10, содержащий 10 первых членов данной прогрессии: A, A + D, A +
 # 2*D, A + 3*D, ... .
 
-def ArifProg(num, diff):
+def ArifProg(num, diff):  # Функция, высчитывающая арифметическую прогрессию.
     counter = 0
     ListArif = []
     while counter != 10:
@@ -10,5 +10,7 @@ def ArifProg(num, diff):
         ListArif.append(summ)
         counter += 1
     return ListArif
+
+# Вывод с вызовом функции.
 
 print(ArifProg(float(input(f"Введите первый член арифметической прогрессии:  ")), float(input(f"Введите разность:  "))))

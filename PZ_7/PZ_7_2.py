85bb237a78486234c81acea66b9010c6a6b59528
Aleksandr <83352964+Sandr-py@users.noreply.github.com>
2021-12-08 23:49:57 +0300

    Update PZ_7_2.py

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -12,11 +12,14 @@ for i in p:
 print(str_list)
 k = 0
 
-for i in range(len(str_list)):
-    if ord(str_list[i]) > ord(str_list[i+1]):
-        print(f"{i+1}-й элемент строки ({str_list[i]}) нарушает алфавитную последовательность")
+reserve = ' '
+
+for i in str_list:
+    if ord(i) < ord(reserve):
+        print(f"{str_list.index(i)}-й элемент строки ({i}) нарушает алфавитную последовательность")
         k += 1
         break
+    else:
+        reserve = i
 if k != 1:
     print("Строка отстортирована в правильном порядке")
-

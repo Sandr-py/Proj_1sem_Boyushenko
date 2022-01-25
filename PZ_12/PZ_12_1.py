db110ce042ef97c29147c03340d936cf7b2dbb11
Sandr-py <aleksandr.boyushenko@mail.ru>
2022-01-26 01:18:56 +0300

    PZ_12 without comments

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -8,92 +8,105 @@ special_list = ['Front-End', 'Back-End', 'Full-Stack']
 
 root = Tk()
 root.title("Анкета Web-Разработчика")
-root.geometry('700x500')
+root.geometry('690x527')
 
 label = Label(text = 'Анкета Web-Разработчика', font = ('Times New Roman', 20))
-label.grid(row = 0, column = 0, sticky = N)
+label.grid(row = 0, column = 0, sticky = N, pady=7)
 var = IntVar()
 
-
-
-
-
-frame1 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame1 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#B0E0E6')
 frame1.grid(row=1, column=0, sticky=W+E+N+S)
-frame2 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame2 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#C0C0C0')
 frame2.grid(row=1, column=1, sticky=W+E+N+S)
-frame3 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame3 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#B0E0E6')
 frame3.grid(row=2, column=0, sticky=W+E+N+S)
-frame4 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame4 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#C0C0C0')
 frame4.grid(row=2, column=1, sticky=N+S+W)
-frame5 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame5 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#B0E0E6')
 frame5.grid(row=3, column=0, sticky=W+E+N+S)
-frame6 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame6 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#C0C0C0')
 frame6.grid(row=3, column=1, sticky=W+E+N+S)
-frame7 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame7 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#B0E0E6')
 frame7.grid(row=4, column=0, sticky=W+E+N+S)
-frame8 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame8 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#C0C0C0')
 frame8.grid(row=4, column=1, sticky=W+E+N+S)
-frame9 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame9 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#B0E0E6')
 frame9.grid(row=5, column=0, sticky=W+E+N+S)
-frame10 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame10 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#C0C0C0')
 frame10.grid(row=5, column=1, sticky=W+E+N+S)
-frame11 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame11 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#B0E0E6')
 frame11.grid(row=6, column=0, sticky=W+E+N+S)
-frame12 = LabelFrame(master=root, relief=RAISED, borderwidth=1)
+frame12 = LabelFrame(master=root, relief=RAISED, borderwidth=1, background='#C0C0C0')
 frame12.grid(row=6, column=1, sticky=W+E+N+S)
 
 
 
-lable1 = Label(master=frame1, text='Регистрационное имя', font=('Times New Roman', 15))
+lable1 = Label(master=frame1, text='Регистрационное имя', font=('Times New Roman', 15), background='#B0E0E6')
 lable1.grid(row=1, column=0, sticky=W+E)
 
-name = Entry(master=frame2, width=40, relief=RAISED, borderwidth=1)
+name = Entry(master=frame2, width=40, relief=RAISED, borderwidth=1, background='#C0C0C0')
 name.grid(row=1, column=1, sticky=W+E+N+S, pady=5, padx=5)
 
-lable2 = Label(master= frame3,text = 'Пароль', font=('Times New Roman', 15))
+lable2 = Label(master= frame3,text = 'Пароль', font=('Times New Roman', 15), background='#B0E0E6')
 lable2.grid(row=2, column=0, sticky=W+N+S+E)
 
-password = Entry(master=frame4, width=40, relief=RAISED, borderwidth=1)
+password = Entry(master=frame4, width=40, relief=RAISED, borderwidth=1, background='#C0C0C0')
 password.grid(row=3, column=1, sticky=W, padx=5)
-double_password = Entry(master=frame4, width=40)
+double_password = Entry(master=frame4, width=40, background='#C0C0C0')
 double_password.grid(rowspan=1, column=1, pady=5, padx=5, sticky=W)
 
-lable_pass = Label(master=frame4, text=':Подтвердите пароль', font=('Times New Roman', 10))
+lable_pass = Label(master=frame4, text=':Подтвердите пароль', font=('Times New Roman', 10), background='#C0C0C0')
 lable_pass.grid(row=4, column=2, sticky=E)
 
-spec_lable = Label(master= frame5,text = 'Ваша специализация', font=('Times New Roman', 15))
+spec_lable = Label(master= frame5,text = 'Ваша специализация', font=('Times New Roman', 15), background='#B0E0E6')
 spec_lable.grid(row=3, column=0, sticky=W)
 
-special = ttk.Combobox(values=special_list)
+special = ttk.Combobox(values=special_list, background='#C0C0C0')
 special.grid(row=3, column=1, sticky=W, padx=5)
 
-gender_lab = Label(master= frame7,text = 'Пол', font=('Times New Roman', 15))
+gender_lab = Label(master= frame7,text = 'Пол', font=('Times New Roman', 15), background='#B0E0E6')
 gender_lab.grid(row=4, column=0, sticky=W)
 
-genderM = Radiobutton(frame8, text='М', variable=var, value='m')
-genderF = Radiobutton(frame8, text='Ж', variable=var, value='f')
+genderM = Radiobutton(frame8, text='М', variable=var, value='m', background='#C0C0C0')
+genderF = Radiobutton(frame8, text='Ж', variable=var, value='f', background='#C0C0C0')
 
 genderM.grid(row=4, column=1)
 genderF.grid(row=4, column=2)
 
-skills = Label(master= frame9,text = 'Ваши навыки', font=('Times New Roman', 15))
+skills = Label(master= frame9,text = 'Ваши навыки', font=('Times New Roman', 15), background='#B0E0E6')
 skills.grid(row=5, column=0, sticky=W)
 
-skill_bar1 = Checkbutton(text='Знание HTML и CSS')
-skill_bar1.grid(in_=frame10,  row=5, column=1, sticky=W, pady=1)
-skill_bar2 = Checkbutton(text='Знание Perl')
+skill_bar1 = Checkbutton(text='Знание HTML и CSS', background='#C0C0C0')
+skill_bar1.grid(in_=frame10, row=5, column=1, sticky=W, pady=1)
+skill_bar2 = Checkbutton(text='Знание Perl', background='#C0C0C0')
 skill_bar2.grid(in_=frame10, row=6, column=1, sticky=W, pady=1)
-skill_bar3 = Checkbutton(text='Знание ASP')
+skill_bar3 = Checkbutton(text='Знание ASP', background='#C0C0C0')
 skill_bar3.grid(in_=frame10, row=7, column=1, sticky=W, pady=1)
-skill_bar4 = Checkbutton(text='Знание Adobe Photoshop')
+skill_bar4 = Checkbutton(text='Знание Adobe Photoshop', background='#C0C0C0')
 skill_bar4.grid(in_=frame10, row=8, column=1, sticky=W, pady=1)
-skill_bar5 = Checkbutton(text='Знание HTML и CSS')
+skill_bar5 = Checkbutton(text='Знание JAVA', background='#C0C0C0')
 skill_bar5.grid(in_=frame10, row=9, column=1, sticky=W, pady=1)
-skill_bar6 = Checkbutton(text='Знание HTML и CSS')
+skill_bar6 = Checkbutton(text='Знание JavaScript', background='#C0C0C0')
 skill_bar6.grid(in_=frame10, row=10, column=1, sticky=W, pady=1)
-skill_bar7 = Checkbutton(text='Знание HTML и CSS')
+skill_bar7 = Checkbutton(text='Знание Flash', background='#C0C0C0')
 skill_bar7.grid(in_=frame10, row=11, column=1, sticky=W, pady=1)
-# Label(master=frame11, text = 'Дополнительные свдения о себе', font=('Times New Roman', 15)).grid(row=6, column=0, sticky=W)
+
+
+more_info = Label(master=frame11, text='Дополнительные сведения о себе', font=('Times New Roman', 15), background='#B0E0E6')
+more_info.grid(row=6, column=0, sticky=W)
+
+inf_txt = Text(height=5, width=43, font=('Times New Roman', 12), wrap=WORD, background='#C0C0C0')
+inf_txt.grid(pady=5, padx=5, row=6, column=1, sticky=W)
+
+scroll = Scrollbar(command=inf_txt.yview, background='#C0C0C0')
+scroll.grid(row=6, column=1, sticky=E+N+S, pady=5, padx=5)
+
+inf_txt.config(yscrollcommand=scroll.set)
+
+log_button = Button(text='Зарегистрировать')
+clear_button = Button(text='Очистить форму')
+
+log_button.grid(pady=7, row=7, column=0, sticky=W)
+clear_button.grid(pady=7, row=7, column=0, sticky=E)
 
 root.mainloop()
--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -1,16 +1,72 @@
-import tkinter as tk
+#
 
-window = tk.Tk()
 
-for i in range(3):
-    for j in range(3):
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
-        frame.grid(row=i, column=j)
-        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-        label.pack()
+from tkinter import *
+from tkinter import ttk
 
-window.mainloop()
+root = Tk()
+root.title('Калькулятор скидки')
+root.geometry('390x190')
+root.configure(bg='#FFF8DC')
+
+def disk_t():
+    a = summa.get()
+    while type(a) != int:  # Обработка исключений
+        try:
+            a = int(a)
+        except ValueError:
+            sale_label.configure(text='Некорректная форма записи')
+            accept_button.configure(command=disk_t)
+            break
+
+    def sale(k, s):  # Составление функции для решения задачи
+        p = s / 100
+        t = k * p
+        sale_label.configure(text=f'{k - t} рублей.')
+
+    if type(a) != int:
+        sale_label.configure(text=f'Некорректная форма записи')
+
+    elif a < 0:
+        sale_label.configure(text=f'Число не должно быть отрицательным')
+    elif 0 <= a < 500:
+        sale(a, 2)
+
+    elif 500 <= a < 1000:
+        sale(a, 3)
+
+    elif 1000 <= a < 1500:
+        sale(a, 4)
+
+    elif 1500 <= a < 2000:
+        sale(a, 5)
+
+    else:
+        sale(a, 10)
+
+
+main_frame = LabelFrame(root, text='Калькулятор скидки', bd=5, bg='#FFF8DC')
+main_frame.grid(pady=5, padx=5)
+
+main_label = Label(master=main_frame, text='Введите стоимость покупки:', font=('Arial', 20), bg='#FFF8DC')
+main_label.grid(row=0, columnspan=2, sticky=W+E, pady=5)
+
+meas_system = Label(master=main_frame, text='Руб.', font=('Arial', 14), bg='#FFF8DC')
+meas_system.grid(row=1, column=1, sticky=W)
+
+accept_button = Button(master=main_frame, text='Принять', font=('Arial', 10), command=disk_t, bg='#F4A460', bd=1,
+                       relief=FLAT, activebackground="#8B4513")
+accept_button.grid(row=2, columnspan=2, sticky=E+W, pady=7)
+
+summa = Entry(master=main_frame, width='20', font=('Arial', 14), bg='#FFDEAD')
+summa.grid(row=1, column=0, sticky=E)
+
+sale_label = Label(main_frame, font=('Arial', 15), bg='#FFDEAD')
+sale_label.grid(row=3, columnspan=2, sticky=E+W)
+
+
+
+
+
+
+root.mainloop()
